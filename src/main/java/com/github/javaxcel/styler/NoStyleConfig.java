e0b5c7a5af221b5d947862ad5493b2d278afe7f1
ImSejin <hackeres123@gmail.com>
2020-11-05 22:14:43 +0900

    ✨ Add: `NoStyleConfig`

---
+++ src/main/java/com/github/javaxcel/styler/NoStyleConfig.java
@@ -0,0 +1,11 @@
+package com.github.javaxcel.styler;
+
+import com.github.javaxcel.styler.config.Configurer;
+
+public class NoStyleConfig implements ExcelStyleConfig {
+
+    @Override
+    public void configure(Configurer configurer) {
+    }
+
+}
